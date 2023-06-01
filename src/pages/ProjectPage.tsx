4020a1aef80457194646beabc59cd3197f66f6b9
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-06-01 19:26:27 +0200

    Removed old code

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -97,27 +97,6 @@ const ViewListItem = ({ item }: ListItemProps) => {
           },
         ]}
       />
-
-      {/* <Menu className="relative" as="div"> */}
-      {/*   <Menu.Button className="flex items-center" as="div"> */}
-      {/*     <EllipsisVerticalIcon className="w-6 h-6 text-white" /> */}
-      {/*   </Menu.Button> */}
-      {/*   <Menu.Items className="flex flex-col gap-2 absolute right-1 min-w-max z-50 rounded bg-slate-500 elevation-4 p-2"> */}
-      {/*     <Menu.Item> */}
-      {/*       <div className="flex px-2 py-1 rounded bg-purple-400 text-white"> */}
-      {/*         <PencilSquareIcon className="w-6 h-6" /> */}
-      {/*         <button className="flex-grow">Edit</button> */}
-      {/*       </div> */}
-      {/*     </Menu.Item> */}
-      {/*     <Menu.Item> */}
-      {/*       <div className="flex px-2 py-1 rounded bg-red-400 text-white"> */}
-      {/*         <TrashIcon className="w-6 h-6" /> */}
-      {/*         <div className="w-2"></div> */}
-      {/*         <button className="">Delete</button> */}
-      {/*       </div> */}
-      {/*     </Menu.Item> */}
-      {/*   </Menu.Items> */}
-      {/* </Menu> */}
     </div>
   );
 };
