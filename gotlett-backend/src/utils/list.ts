1e830a5c22ecac9bb93ee69152c46fa8aa2edba6
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-07-11 20:06:00 +0200

    Gotlett: Implemented reordering of lists

--- gotlett-backend/src/utils/list.ts
+++ gotlett-backend/src/utils/list.ts
@@ -78,8 +78,82 @@ export async function moveItemToList(
   }
 
   if (beforeId) {
+    // Get the full item
+    const beforeItem = await prisma.projectListItem.findUnique({
+      where: { id: beforeId },
+    });
+
+    if (!beforeItem || beforeItem.listId != listId) {
+      throw new TRPCError({ code: "BAD_REQUEST" });
+    }
+
     // We need to reorder the list
-    throw new TRPCError({ code: "METHOD_NOT_SUPPORTED" });
+    const sourceListItems = await prisma.projectListItem.findMany({
+      where: { listId: item.listId },
+      orderBy: { index: "asc" },
+    });
+
+    const destListItems = await prisma.projectListItem.findMany({
+      where: { listId },
+      orderBy: { index: "asc" },
+    });
+
+    const sourceIndex = sourceListItems.findIndex((i) => i.id == item.id);
+    const destIndex = destListItems.findIndex((i) => i.id == beforeItem.id);
+
+    console.log("Before Source", sourceListItems);
+    console.log("Before Dest", destListItems);
+
+    const [old] = sourceListItems.splice(sourceIndex, 1);
+    destListItems.splice(destIndex, 0, old);
+
+    console.log("Item", item.name);
+    console.log("Before", beforeItem.name);
+
+    console.log("Source", sourceListItems);
+    console.log("Dest", destListItems);
+
+    // List of prisma transactions with new item index
+    const updatedItems = [];
+    updatedItems.push(
+      prisma.projectListItem.update({
+        where: { id: item.id },
+        data: { listId },
+      }),
+    );
+
+    for (let index = 0; index < destListItems.length; index++) {
+      const item = destListItems[index];
+      // If the item index dont match then we need to update them
+      if (item.index != index) {
+        // Push on the new updated item transaction
+        updatedItems.push(
+          prisma.projectListItem.update({
+            where: { id: item.id },
+            data: { index },
+          }),
+        );
+      }
+
+      for (let i = 0; i < sourceListItems.length; i++) {
+        const item = sourceListItems[i];
+        // If the item index dont match then we need to update them
+        if (item.index != i) {
+          // Set the new index
+          item.index = i;
+          // Push on the new updated item transaction
+          updatedItems.push(
+            prisma.projectListItem.update({
+              where: { id: item.id },
+              data: { index: item.index },
+            }),
+          );
+        }
+      }
+
+      // Wait for all the transactions
+      await prisma.$transaction(updatedItems);
+    }
   } else {
     // Just add the item to the end of the list
     const count = await prisma.projectListItem.count({
