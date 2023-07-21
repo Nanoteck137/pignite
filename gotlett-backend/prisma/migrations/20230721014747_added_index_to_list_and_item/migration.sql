2fb33668a98ce1a331d801d01c6c09f0056557f7
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-07-21 03:55:36 +0200

    Gotlett: Created migration for the reordering of lists and items

---
+++ gotlett-backend/prisma/migrations/20230721014747_added_index_to_list_and_item/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ProjectList" ADD COLUMN     "index" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "ProjectListItem" ADD COLUMN     "index" INTEGER NOT NULL DEFAULT 0;
