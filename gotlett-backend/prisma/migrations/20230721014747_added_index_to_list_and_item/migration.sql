418e8002ea1b4f7d3e0d77ef90b4ad8809205bf3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-20 07:00:48 +0100

    Created bun project

--- gotlett-backend/prisma/migrations/20230721014747_added_index_to_list_and_item/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ProjectList" ADD COLUMN     "index" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "ProjectListItem" ADD COLUMN     "index" INTEGER NOT NULL DEFAULT 0;
